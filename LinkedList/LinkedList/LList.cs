b511b828af71d0076d0f2450676420d833eac18c
Sean <shoang1550183@gmail.com>
2021-08-15 20:35:05 +0100

    Bigram count done.

--- LinkedList/LinkedList/LList.cs
+++ LinkedList/LinkedList/LList.cs
@@ -7,6 +7,39 @@ namespace LinkedList
     class LList<T> : ILinkedList<T>
     {
         ListElement<T> _firstElement;
+        public void PrintBigrams()
+        {
+            var bigramDict = GetBigramDict();
+            foreach(KeyValuePair<string, int> bigram in bigramDict)
+            {
+                Console.WriteLine(bigram.Key + " - " + bigram.Value);
+            }
+        }
+        public Dictionary<string, int> GetBigramDict()
+        {
+            Dictionary<string, int> bigramDictionary = new Dictionary<string, int>();
+            var currentElement = _firstElement;
+            while (currentElement != null)
+            {
+                for (int x = 0; x < currentElement.value.ToString().Length - 1; x++)
+                {
+                    var currentBigramCheck = currentElement.value.ToString().Substring(x, 2);
+                    currentBigramCheck = currentBigramCheck.ToLower();
+                    currentBigramCheck = currentBigramCheck.Replace(" ", "_");
+                    if (bigramDictionary.ContainsKey(currentBigramCheck))
+                    {
+                        bigramDictionary.TryGetValue(currentBigramCheck, out int bigramCount);
+                        bigramDictionary[currentBigramCheck] = bigramCount + 1;
+                    }
+                    else
+                    {
+                        bigramDictionary.Add(currentBigramCheck, 1);
+                    }
+                }
+                currentElement = currentElement.next;
+            }
+            return bigramDictionary;
+        }
         public void Add(int position, T data)
         {
             // at position
--- LinkedList/LinkedList/Program.cs
+++ LinkedList/LinkedList/Program.cs
@@ -6,36 +6,42 @@ namespace LinkedList
     {
         static void Main(string[] args)
         {
-            LList<string> myList = new LList<string>();
+            //LList<string> myList = new LList<string>();
 
-            myList.Add("1st");
-            myList.Add("2nd");
-            myList.Add("3rd");
-            myList.Add("4th");
-            myList.PrintAll();
-            Console.WriteLine();
+            //myList.Add("1st");
+            //myList.Add("2nd");
+            //myList.Add("3rd");
+            //myList.Add("4th");
+            //myList.PrintAll();
+            //Console.WriteLine();
 
-            myList.Add(0, "NEW 1");
-            myList.PrintAll();
-            Console.WriteLine();
+            //myList.Add(0, "NEW 1");
+            //myList.PrintAll();
+            //Console.WriteLine();
 
-            myList.Add("4th", "NEW 2");
-            myList.PrintAll();
-            Console.WriteLine();
+            //myList.Add("4th", "NEW 2");
+            //myList.PrintAll();
+            //Console.WriteLine();
 
-            myList.Delete(4);
-            myList.PrintAll();
-            Console.WriteLine();
+            //myList.Delete(4);
+            //myList.PrintAll();
+            //Console.WriteLine();
 
-            myList.Delete("3rd");
-            myList.PrintAll();
-            Console.WriteLine();
+            //myList.Delete("3rd");
+            //myList.PrintAll();
+            //Console.WriteLine();
 
-            myList.Replace("NEW 1", "REPLACED");
-            myList.PrintAll();
-            Console.WriteLine();
+            //myList.Replace("NEW 1", "REPLACED");
+            //myList.PrintAll();
+            //Console.WriteLine();
 
-            Console.WriteLine(myList.Get(0).value);
+            //Console.WriteLine(myList.Get(0).value);
+
+            LList<string> bigramList = new LList<string>();
+            bigramList.Add("Sean Sean");
+            bigramList.Add("Sean Sean");
+            bigramList.Add("Broseph");
+            bigramList.PrintBigrams();
         }
     }
 }
