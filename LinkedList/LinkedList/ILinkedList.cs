4fe09645d708e429b63fc384921806585bb6f233
Sean <shoang1550183@gmail.com>
2021-08-21 19:04:01 +0100

    Revert "Revert "Enumerable changes. Bigram split""
    This reverts commit 2db6a55ed0a7af6c71bff8344bbdf2a4af03c42c.

--- LinkedList/LinkedList/ILinkedList.cs
+++ LinkedList/LinkedList/ILinkedList.cs
@@ -1,27 +1,28 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 
 namespace LinkedList
 {
-    public interface ILinkedList<T>
+    public interface ILinkedList<T>: IEnumerable
     {
         /// <summary>
         /// add at position
         /// </summary>
-        void Add(int postion, T data);
+        void AddAtPos(int postion, T data);
         /// <summary>
         /// add after data
         /// </summary>
-        void Add(T dataToAddAfter, T nextData);
+        void AddAfterData(T dataToAddAfter, T nextData);
         /// <summary>
         /// add at start
         /// </summary>
-        void Add(T data);
+        void AddAtStart(T data);
 
 
-        void Delete(int postion);
-        void Delete(T data);
+        void DeletePos(int postion);
+        void DeleteData(T data);
 
         ListElement<T> Get(int position);
 
