ba6dbc649c65aa695e8af077dede5aa560b79437
Sean <shoang1550183@gmail.com>
2021-08-21 21:32:25 +0100

    No yield ver Pnl csv

---
+++ LinkedList/Pnl/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Pnl
+{
+    class Program
+    {
+        static List<StrategyPnl> StrategyList;
+        static void Main(string[] args)
+        {
+            // strategy columns (strat name + all Pnls in them) = list in StrategyList (stratpnl)
+            // Pnls have a date + value
+            var streamReader = File.OpenText("pnl.csv");
+            StrategyList = InitStrategyList(streamReader);
+            InitStrategyList(streamReader);
+            PopulateStrategyList(streamReader);
+            PrintStrategyPnls(15);
+        }
+
+        private static void PopulateStrategyList(StreamReader streamReader)
+        {
+            while (!streamReader.EndOfStream)
+            {
+                string currentLine = streamReader.ReadLine();
+                var values = currentLine.Split(",");
+                DateTime currentDate = DateTime.Parse(values[0]);
+                for (int x = 1; x < values.Length; x++)
+                {
+                    Pnl newPnl = new Pnl(currentDate, decimal.Parse(values[x]));
+                    StrategyList[x - 1].Pnls.Add(newPnl);
+                }
+            }
+        }
+
+        private static List<StrategyPnl> InitStrategyList(StreamReader streamReader)
+        {
+            List<StrategyPnl> stratPanelList = new List<StrategyPnl>();
+            string[] columnHeaders = streamReader.ReadLine().Split(",");
+            foreach (string column in columnHeaders.Skip(1))
+            {
+                StrategyPnl newSP = new StrategyPnl(column);
+                stratPanelList.Add(newSP);
+            }
+            return stratPanelList;
+        }
+
+        public static void PrintStrategyPnls(int strategyNumber)
+        {
+            if (strategyNumber > 15)
+            {
+                throw new Exception("Out of bounds");
+            }
+            foreach (Pnl pnl in StrategyList[strategyNumber - 1].Pnls)
+            {
+                Console.WriteLine(pnl.ToString());
+            }
+        }
+    }
+}
---
+++ LinkedList/Pnl/StrategyPnl.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pnl
+{
+    class StrategyPnl
+    {
+        public StrategyPnl(string newStrategy)
+        {
+            Strategy = newStrategy;
+            Pnls = new List<Pnl>();
+        }
+        public string Strategy { get; set; }
+        public List<Pnl> Pnls { get; set; } 
+    }
+}
