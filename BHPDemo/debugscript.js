44ba1e2852d4a64a692fece86e34328d2ee0be56
Sean <shoang1550183@gmail.com>
2021-10-28 16:49:13 +0100

    second set of functions

--- BHPDemo/debugscript.js
+++ BHPDemo/debugscript.js
@@ -1,10 +1,10 @@
 
-var btnDebug = document.getElementById("btnDebug");
-var lblDebug = document.getElementById("lblDebug");
-var txtDebug = document.getElementById("txtDebug");
+let btnDebug = document.getElementById("btnDebug");
+let lblDebug = document.getElementById("lblDebug");
+let txtDebug = document.getElementById("txtDebug");
 
 function debugButtonClicked() {
-    var input = "";
+    let input = "";
     input = txtDebug.value;
     //debugger;
     lblDebug.innerText = "You have clicked the button";
@@ -12,16 +12,74 @@ function debugButtonClicked() {
 }
 
 function changeLabelText(text) {
-    var labelDisplay = "";
+    let labelDisplay = "";
     labelDisplay = "Your input is: " + text;
     lblDebug.innerText = labelDisplay;
 }
 
 function buggedClick() {
-    var firstNumber = 1;
-    var secondNumber = "2 ";
-    var label = document.getElementById("lblBugged");
+    // should be 3
+    let firstNumber = 1;
+    let secondNumber = "2";
+    let label = document.getElementById(lblBugged);
 
-    var combine = firstNumber + secondNumber;
+    let combine = firstNumber + secondNumber;
     label.innerHTML = combine;
+}
+
+function bugFix(number) {
+    return parseInt(number);
+}
+
+function elseIfLoop() {
+    let forString = "";
+    for (let x = 0; x < 10; x++) {
+        forString += elseIfValues(x);
+    }
+}
+function elseIfValues(value) {
+    if (value === 9) {
+        return "!";
+    } else if (value === 11) {
+        return "o";
+    }
+    else if (value === 5) {
+        return "g";
+    }
+    else if (value === 4) {
+        return "g";
+    }
+    else if (value === 8) {
+        return "g";
+    }
+    else if (value === 1) {
+        return "e";
+    }
+    else if (value === 12) {
+        return "z";
+    }
+    else if (value === 7) {
+        return "n";
+    }
+    else if (value === 0) {
+        return "d";
+    }
+    else if (value === 6) {
+        return "i";
+    }
+    else if (value === 13) {
+        return "y";
+    }
+    else if (value === 1) {
+        return "e";
+    }
+    else if (value === 3) {
+        return "u";
+    }
+    else if (value === 10) {
+        return "!";
+    }
+    else if (value === 2) {
+        return "b";
+    }
 }
