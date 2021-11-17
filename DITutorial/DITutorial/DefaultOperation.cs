354cdaec9fd5eeb41fd4b98b31b2d0998ea16995
Sean <shoang1550183@gmail.com>
2021-11-17 20:26:27 +0000

    DI Tutorial

---
+++ DITutorial/DITutorial/DefaultOperation.cs
@@ -0,0 +1,13 @@
+﻿using DITutorial.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Guid;
+
+namespace DITutorial
+{
+    class DefaultOperation : ITransientOperation, IScopedOperation, ISingletonOperation
+    {
+        public string OperationId { get; } = NewGuid().ToString()[^4..];
+    }
+}
---
+++ DITutorial/DITutorial/Interfaces/IOperation.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DITutorial.Interfaces
+{
+    public interface IOperation
+    {
+        string OperationId { get; }
+    }
+}
