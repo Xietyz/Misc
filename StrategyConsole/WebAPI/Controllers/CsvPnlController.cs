0abf714522e2e02686332b7ddefd59c0a9699a00
Sean <shoang1550183@gmail.com>
2021-11-21 01:45:23 +0000

    Started

---
+++ StrategyConsole/WebAPI/Controllers/CsvPnlController.cs
@@ -0,0 +1,32 @@
+﻿using CsvPnl.Database;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class CsvPnlController : ControllerBase
+    {
+        // https://stackoverflow.com/questions/62411410/is-there-any-better-way-to-add-the-dbcontext-to-a-asp-core-mvc-controller
+        private readonly ILogger<CsvPnlController> _logger;
+        private readonly PnldbContext _dbContext;
+
+        public CsvPnlController(ILogger<CsvPnlController> logger, PnldbContext context)
+        {
+            _logger = logger;
+            context = _dbContext;
+        }
+
+        [HttpGet]
+        [Route("cumulative-pnl")]
+        public IEnumerable<Pnl> Get()
+        {
+            
+        }
+    }
+}
